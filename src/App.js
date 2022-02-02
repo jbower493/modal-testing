3cd7a29d58fddf9b4aad4efb1283d250d1ade473
Jamie Bower <jbower@nexigen.digital>
2022-02-02 11:58:37 +1100

    feat: added excess space component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import ReactResponsiveModal from './modals/reactResponsiveModal';
 import OwnWithBlock from './modals/ownWithBlock';
 import OwnWithoutBlock from './modals/ownWithoutBlock';
 
+import ExcessSpace from './excessSpace';
+
 function App() {
     return (
         <div className="app">
@@ -19,12 +21,14 @@ function App() {
                         <Link to="/react-modal">React Modal</Link>
                         <Link to="/own-without-block">Own Without Block</Link>
                         <Link to="/own-with-block">Own With Block</Link>
+                        <Link to="/excess-space">iPhone excess space</Link>
                     </div>} />
                     <Route path="/reach" element={<div><Background render={(open, onClose) => <Reach open={open} onClose={onClose} />} name="Reach" /></div>} />
                     <Route path="/react-responsive-modal" element={<div><Background render={(open, onClose) => <ReactResponsiveModal open={open} onClose={onClose} />} name="React Responsive Modal" /></div>} />
                     <Route path="/react-modal" element={<div><Background render={(open, onClose) => <ReactModal open={open} onClose={onClose} />} name="React Modal" /></div>} />
                     <Route path="/own-without-block" element={<div><Background render={(open, onClose) => <OwnWithoutBlock open={open} onClose={onClose} />} name="Own Without Block" /></div>} />
                     <Route path="/own-with-block" element={<div><Background render={(open, onClose) => <OwnWithBlock open={open} onClose={onClose} />} name="Own With Block" /></div>} />
+                    <Route path="/excess-space" element={<ExcessSpace />} />
                 </Routes>
             </BrowserRouter>
         </div>
---
+++ src/excessSpace.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ExcessSpace = () => (
+    <section>
+        There is some weird space at the bottom of the iphone screen on my modals
+    </section>
+);
+
+export default ExcessSpace;
